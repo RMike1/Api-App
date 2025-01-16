688b782303aaf75fa1d2abf6b7a47d488635a68b
RMike1 <71224316+RMike1@users.noreply.github.com>
2025-01-16 14:26:18 +0200

    adding more assertions..

--- tests/Feature/LogoutTest.php
+++ tests/Feature/LogoutTest.php
@@ -2,16 +2,23 @@
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Sanctum\Sanctum;
 
 uses(RefreshDatabase::class);
+beforeEach(
+    fn() =>
+    $this->user = User::factory()->create()
+);
 
 it('logs out the authenticated user', function () {
-    $user = User::factory()->create();
-    $token = $user->createToken('TestToken')->plainTextToken;
-    $this->withHeaders([
-        'Authorization' => 'Bearer ' . $token,
-    ])->postJson('/api/logout')
-    ->assertStatus(200)
-    ->assertJson(['message' => 'Successfully logged out...']);
-    $this->assertCount(0, $user->tokens);
+    Sanctum::actingAs($this->user);
+    $this->postJson('/api/logout')
+        ->assertStatus(200)
+        ->assertJson(['message' => 'Successfully logged out...']);
+    $this->assertCount(0, $this->user->tokens);
+    $this->assertGuest('api');
 });
+
+it('fails when the user is not authenticated')->postJson('/api/logout')
+    ->assertStatus(401)
+    ->assertJson(['message' => 'Unauthenticated.']);
